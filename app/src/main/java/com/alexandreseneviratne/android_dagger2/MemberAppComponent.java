1f7cdc1a2a1f359ffd1307ad513799280aaabc46
alex <aleandre.seneviratne@gmail.com>
2018-10-31 14:02:10 +0530

    Add @Singleton

--- app/src/main/java/com/alexandreseneviratne/android_dagger2/MemberAppComponent.java
+++ app/src/main/java/com/alexandreseneviratne/android_dagger2/MemberAppComponent.java
@@ -1,11 +1,14 @@
 package com.alexandreseneviratne.android_dagger2;
 
+import javax.inject.Singleton;
+
 import dagger.Component;
 
 /**
  * Created by Alexandre SENEVIRATNE on 31/10/2018.
  */
 
+@Singleton
 @Component(modules = MemberDataModule.class)
 public interface MemberAppComponent {
     void inject(MainActivity mainActivity);
